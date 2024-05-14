b35d405e90f9905525eb0daaaf3ea2651cb10faa
chock1991 <chochristinak@gmail.com>
2024-05-13 22:11:31 -0600

    fix

--- server.js
+++ server.js
@@ -1,50 +1,46 @@
-var createError = require('http-errors');
-var express = require('express');
+var createError = require("http-errors");
+var express = require("express");
 
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var session = require('express-session');
-var passport = require('passport');
-var methodOverride = require('method-override');
-require('dotenv').config();
-require('./config/database');
-require('./config/passport');
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var session = require("express-session");
+var passport = require("passport");
+var methodOverride = require("method-override");
+require("dotenv").config();
+require("./config/database");
+require("./config/passport");
 
-
-
-
-const indexRouter = require('./routes/index');
-const goalsRouter = require('./routes/goals');
-const journalsRouter = require('./routes/journals');
-const journalPromptsRouter = require('./routes/journalprompts');
-const gainsRouter = require('./routes/gains');
+const indexRouter = require("./routes/index");
+const goalsRouter = require("./routes/goals");
+const journalsRouter = require("./routes/journals");
+const journalPromptsRouter = require("./routes/journalPrompts");
+const gainsRouter = require("./routes/gains");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 app.use(function (req, res, next) {
-  console.log('What is the current moon phase')
-  res.locals.date = new Date().toLocaleDateString()
-  next()
-})
-app.use(logger('dev'));
+  console.log("What is the current moon phase");
+  res.locals.date = new Date().toLocaleDateString();
+  next();
+});
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-
-app.use(session({
-  secret: process.env.SECRET,
-  resave: false,
-  saveUninitialized: true
-}));
-app.use(methodOverride('_method'));
-
-
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(methodOverride("_method"));
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -52,26 +48,24 @@ app.use(function (req, res, next) {
   res.locals.user = req.user;
   next();
 });
-app.use('/', indexRouter);
-app.use('/goals', goalsRouter);
-app.use('/', journalsRouter);
-app.use('/journalprompts', journalPromptsRouter)
-app.use('/gains', gainsRouter)
+app.use("/", indexRouter);
+app.use("/goals", goalsRouter);
+app.use("/", journalsRouter);
+app.use("/journalprompts", journalPromptsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
