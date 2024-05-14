36915a61ba59a2850a4423abd9363d380fa1b19c
chock1991 <chochristinak@gmail.com>
2024-05-13 21:53:03 -0600

    Fix show done goals function

--- controllers/gains.js
+++ controllers/gains.js
@@ -5,17 +5,12 @@ module.exports = {
 };
 
 async function showGains(req, res) {
-  try {
-    const goal = await Goal.findById(req.params.id);
-    const doneGoals = await Goal.find({ accomplished: true }).populate(
-      "doneList"
-    );
-    res.render("gains/show", {
-      title: "Celebrate Your Achievements",
-      doneGoals,
-    });
-  } catch (error) {
-    console.error("Error retrieving done goals:", error);
-    res.sendStatus(500);
+    try {
+      const doneGoals = await Goal.find({ 'accomplished': true }).populate('doneList');
+  
+      res.render('gains/show', { title: 'Celebrate Your Achievements', doneGoals });
+    } catch (error) {
+      console.error('Error retrieving done goals:', error);
+      res.sendStatus(500);
+    }
   }
-}
